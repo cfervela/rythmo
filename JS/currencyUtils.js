59f2d3bff4bdc607ec4bffd991f2499147b591cb
Fernanda Vela <fer.vela2703@gmail.com>
2025-12-04 10:12:08 -0600

    Changing localhost:3000 urls

--- JS/currencyUtils.js
+++ JS/currencyUtils.js
@@ -4,7 +4,7 @@
 const CURRENCY_API_URL =
   typeof API_BASE_URL !== "undefined"
     ? API_BASE_URL
-    : "http://localhost:3000/api";
+    : "https://web-5lecz6bm76nn.up-de-fra1-k8s-1.apps.run-on-seenode.com/";
 
 // Tasas de conversión desde MXN (sincronizadas con el backend)
 const exchangeRates = {
